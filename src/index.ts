d2f3967432a3849c5930f3b938a1e58cccf49772
Emanuel Ledesma <eledesma@griddynamics.com>
2025-07-07 17:28:44 -0500

    Add middleware to be able to submit the form from browser

--- src/index.ts
+++ src/index.ts
@@ -13,17 +13,20 @@ const app = express();
 
 const init = async () => {
   console.log('Initializing the app...');
+  app.use(express.urlencoded({ extended: true }));
+
   const db = await initializeDB();
   runMigrations(db);
-
-  const userModel = new UserModel(db);
-  const userController = new UserController(userModel);
 
   app.use(cors());
   app.use(express.static('public'));
   app.get('/', (req: Request, res: Response) => {
     res.sendFile('views/index.html', { root: path.join(__dirname, '../') });
   });
+
+  const userModel = new UserModel(db);
+  const userController = new UserController(userModel);
+
   app.use('/api', createApiRoutes(userController));
 };
 
