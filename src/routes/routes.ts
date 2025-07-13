0a1984a02b3e1f4ab3480fc93b0c0e34b01de339
Emanuel Ledesma <eledesma@griddynamics.com>
2025-07-13 17:50:29 -0500

    Edit get all users route to use user controller

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -7,15 +7,7 @@ export function createApiRoutes(
   const router = Router();
 
   router.post('/users', userController.createUser.bind(userController));
-
-  router.get('/users', (req, res) => {
-    // Simulate fetching users from a database
-    const users = [
-      { id: 1, name: 'John Doe' },
-      { id: 2, name: 'Jane Smith' },
-    ];
-    res.json(users);
-  });
+  router.get('/users', userController.getUsers.bind(userController));
 
   return router;
 }
