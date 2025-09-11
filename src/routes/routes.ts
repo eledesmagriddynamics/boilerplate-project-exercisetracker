37aef254197b4b4284a593ce6c408525942cb6b3
eledesmagriddynamics <eledesma@griddynamics.com>
2025-09-11 11:22:41 -0600

    Add user existence middleware
    Introduced a checkUserExists middleware to validate user existence before creating exercises. Refactored ExerciseController to use user data from the request, updated route setup to include the middleware, and added relevant request and user interfaces for type safety.

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,16 +1,23 @@
-import { Router } from 'express';
+import { RequestHandler, Router } from 'express';
 import { UserController } from '../controllers/userController';
 import { ExerciseController } from '../controllers/exerciseController';
+import { checkUserExists } from '../middlewares/checkUserExists';
+import { UserModel } from '../models/userModel';
 
 export function createApiRoutes(
   userController: UserController,
-  exerciseController: ExerciseController
+  exerciseController: ExerciseController,
+  userModel: UserModel,
 ) {
   const router = Router();
 
   router.post('/users', userController.createUser.bind(userController));
   router.get('/users', userController.getUsers.bind(userController));
-  router.post('/users/:_id/exercises', exerciseController.createExercise.bind(exerciseController));
+  router.post(
+    '/users/:_id/exercises',
+    checkUserExists(userModel) as unknown as RequestHandler,
+    exerciseController.createExercise.bind(exerciseController) as unknown as RequestHandler,
+  );
 
   return router;
 }
