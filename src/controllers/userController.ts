0d7e7567185422b209745dce32f5f27b5043b5c9
eledesmagriddynamics <eledesma@griddynamics.com>
2025-09-11 12:17:43 -0600

    Add request body validation middleware
    Introduced a reusable validateBody middleware using Zod for validating request bodies. Updated user and exercise creation routes to use this middleware, removing inline schema parsing from controllers for cleaner separation of concerns.

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,14 +1,12 @@
 import { Request, Response } from 'express';
 import { UserModel } from '../models/userModel';
-import { userSchema } from '../validations/schema';
 
 export class UserController {
   constructor(private userModel: UserModel) {}
 
   async createUser(req: Request, res: Response) {
     try {
-      console.log('Creating user with data:', req.body);
-      const { username } = userSchema.parse(req.body);
+      const { username } = req.body;
       const user = await this.userModel.createUser(username);
       res.status(201).json(user);
     } catch (error) {
